8d25e32d2e74931d4c3d52d59223baaa11c85329
xinykou <xinyi@stu.ecnu.edu.cn>
2024-06-03 21:48:50 +0800

    LLaMA-Factory upload

---
+++ LLaMA-Factory/data/safety/alignment/BeaverTails_preference.py
@@ -0,0 +1,30 @@
+from datasets import load_dataset
+import os
+import json
+# 设置HTTP代理
+os.environ["HTTP_PROXY"] = "http://127.0.0.1:27999"
+os.environ["HTTPS_PROXY"] = "http://127.0.0.1:27999"
+
+
+data_size = 2000
+
+if data_size == 2000:
+    output_json = f'BeaverTails_preference-2k.json'
+else:
+    raise ValueError("data_size must be 2000")
+
+dataset =load_dataset("PKU-Alignment/PKU-SafeRLHF-30K", cache_dir="../../../../data/cache")
+
+index = 0
+list_data_dict =[]
+for example in dataset["30k_train"]:
+    if example["is_safe"] and index < 2000:
+        instance = {}
+        instance["output"] = example["response"]
+        instance["instruction"] = example["prompt"]
+        instance["input"] = ""
+        list_data_dict += [instance]
+        index += 1
+
+with open(output_json, 'w', encoding='utf-8') as f:
+    json.dump(list_data_dict, f, indent=4)
