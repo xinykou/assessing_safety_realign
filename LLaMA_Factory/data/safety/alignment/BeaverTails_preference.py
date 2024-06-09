9f60bc913cec946940009d63baddc533cfbefd1e
xinykou <xinyi@stu.ecnu.edu.cn>
2024-06-09 14:31:14 +0800

    build preference dataset

--- LLaMA_Factory/data/safety/alignment/BeaverTails_preference.py
+++ LLaMA_Factory/data/safety/alignment/BeaverTails_preference.py
@@ -1,11 +1,12 @@
 from datasets import load_dataset
 import os
 import json
+
 # 设置HTTP代理
 os.environ["HTTP_PROXY"] = "http://127.0.0.1:27999"
 os.environ["HTTPS_PROXY"] = "http://127.0.0.1:27999"
 
-
+# ----2k---------------------------------------------------------------------------------
 data_size = 2000
 
 if data_size == 2000:
@@ -20,7 +21,7 @@ list_data_dict = []
 for example in dataset["train"]:  # 26.9k examples
     if (example["is_response_0_safe"] and not example["is_response_1_safe"]) or \
             (not example["is_response_0_safe"] and example["is_response_1_safe"]):
-        if index < 2000:
+        if index < data_size:
             instance = {}
             instance["instruction"] = example["prompt"]
             instance["input"] = ""
@@ -33,9 +34,28 @@ for example in dataset["train"]:  # 26.9k examples
 
             list_data_dict += [instance]
             index += 1
-        else:
-            break
 
 with open(output_json, 'w', encoding='utf-8') as f:
     json.dump(list_data_dict, f, indent=4)
 
+# ----10k---------------------------------------------------------------------------------
+# output_json = f'BeaverTails_preference-10k.json'
+# dataset = load_dataset("PKU-Alignment/PKU-SafeRLHF-10K", cache_dir="../../../../data/cache")
+#
+# list_data_dict = []
+# for example in dataset["train"]:
+#     # if (example["is_response_0_safe"] and not example["is_response_1_safe"]) or \
+#     #         (not example["is_response_0_safe"] and example["is_response_1_safe"]):
+#     instance = {}
+#     instance["instruction"] = example["prompt"]
+#     instance["input"] = ""
+#     responses = [example["response_0"], example["response_1"]]
+#     chosen_id = example["safer_response_id"]
+#     rejected_id = 1 - chosen_id
+#     instance["chosen"] = responses[chosen_id]
+#     instance["rejected"] = responses[rejected_id]
+#
+#     list_data_dict += [instance]
+#
+# with open(output_json, 'w', encoding='utf-8') as f:
+#     json.dump(list_data_dict, f, indent=4)
