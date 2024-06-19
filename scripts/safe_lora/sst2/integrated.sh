c6f5662f569af88773cfa6f9a137b4672e885f8d
xinykou <xinyi@stu.ecnu.edu.cn>
2024-06-19 22:12:59 +0800

    baseline: safe_lora

---
+++ scripts/safe_lora/sst2/integrated.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+
+cd $current_script_dir
+echo "current_script_path: $current_script_dir"
+# 为每个脚本添加执行权限并执行它们
+
+# Call mask-realign_dpo.sh
+chmod +x ./mask-realign_dpo.sh
+./mask-realign_dpo.sh
+if [ $? -ne 0 ]; then
+  echo "mask-realign_dpo.sh failed"
+  exit 1
+fi
+
+# Call mask-eval_safety.sh
+chmod +x ./mask-eval_safety.sh
+./mask-eval_safety.sh
+if [ $? -ne 0 ]; then
+  echo "mask-eval_safety.sh failed"
+  exit 1
+fi
+
+# Call mask-eval_downstream.sh
+chmod +x ./mask-eval_downstream.sh
+./mask-eval_downstream.sh
+if [ $? -ne 0 ]; then
+  echo "mask-eval_downstream.sh failed"
+  exit 1
+fi
---
+++ scripts/safe_lora/sst2/realign_dpo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts/safet_lora
+sub_dir=$(dirname "$parent_dir") # ./scripts
+main_dir=$(dirname "$sub_dir") # ./
+
+cd $main_dir
+
+# build alignment matrix
+export CUDA_VISIBLE_DEVICES=0,1
+
+
+dataset_name="sst2"
+dataset_selected="n1000_p0.05"
+
+
+# realign lora
+for tau in $(seq 0.1 0.1 1.0); do
+    echo "Running with tau=$tau"
+    python ./safe_lora/identify_realign.py \
+         --model_path ./saves/lora/sft/checkpoint-125-merged \
+         --lora_path ./saves/lora/finetune/${dataset_name}-dpo/${dataset_selected} \
+         --aligned_path ./saves/lora/dpo \
+         --realign_type scale \
+         --output_path ./saves/lora/safe_lora/${dataset_name}-dpo-${dataset_selected} \
+         --tau ${tau} \
+
+done
+
