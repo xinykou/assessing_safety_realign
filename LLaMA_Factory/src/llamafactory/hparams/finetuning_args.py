0dbbbe07e65e24c7e93fd103a8cf2f0cf58a8678
xinykou <xinyi@stu.ecnu.edu.cn>
2024-06-30 20:58:26 +0800

    args

--- LLaMA_Factory/src/llamafactory/hparams/finetuning_args.py
+++ LLaMA_Factory/src/llamafactory/hparams/finetuning_args.py
@@ -323,6 +323,10 @@ class FinetuningArguments(FreezeArguments, LoraArguments, RLHFArguments, GaloreA
         default=False,
         metadata={"help": "Whether or not to save the training loss curves."},
     )
+    methods_name: str = field(
+        default="",
+        metadata={"help": "other baselines against fine-tuning attack, such as vaccine, lisa, ..."},
+    )
 
     def __post_init__(self):
         def split_arg(arg):
