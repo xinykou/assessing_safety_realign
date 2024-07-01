ddad01be766ec810d89fb0e3c56275fb0fb63cb6
xinykou <xinyi@stu.ecnu.edu.cn>
2024-07-01 21:29:32 +0800

    alignment_techniques at 'vlguard'

---
+++ scripts/baselines/alignment_techniques/integrated_eval-vlguard_finetune.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts/alignment
+sub_dir=$(dirname "$parent_dir") # scripts
+main_dir=$(dirname "$sub_dir") # ./
+
+cd $main_dir
+
+chmod +x ./scripts/baselines/alignment_techniques/vlguard_finetune.sh
+
+./scripts/baselines/alignment_techniques/vlguard_finetune.sh
+
+
+params=(
+      "2 vlguard-finetune-kto n1000_p0.05"
+      "3 vlguard-finetune-simpo n1000_p0.05"
+      "4 vlguard-finetune-sft n1000_p0.05"
+      "5 vlguard-finetune-orpo n1000_p0.05"
+)
+
+
+chmod +x ./scripts/baselines/alignment_techniques/eval_parallel.sh
+
+# Run the script in parallel
+for param in "${params[@]}"
+do
+    ./scripts/baselines/alignment_techniques/eval_parallel.sh $param &
+done
+
+wait
+
+echo "All eval done!"
---
+++ scripts/baselines/alignment_techniques/vlguard_finetune.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts /baselines
+sub_dir=$(dirname "$parent_dir") # ./scripts
+subsub_dir=$(dirname "$sub_dir") # .
+
+
+cd $subsub_dir
+echo "Current working directory: $subsub_dir"
+
+export WANDB_PROJECT="assessing_safety"
+
+export PYTHONPATH=$subsub_dir
+export CUDA_VISIBLE_DEVICES=4,5
+
+
+main_name=vlguard_finetune
+alignment_methods=("sft" "simpo" "orpo" "kto")  # 0.01 0.05 0.1 0.2 0.3
+
+# shellcheck disable=SC2068
+for align_m in ${alignment_methods[@]}; do
+    echo "The alignment method is: ${align_m}"
+
+## finetune for downstream task
+python main.py train config/baselines/alignment_techniques/${main_name}-"${align_m}"-n1000_p0.05.yaml
+done
