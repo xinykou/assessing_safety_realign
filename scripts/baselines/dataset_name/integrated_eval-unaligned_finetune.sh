0cf62b360f482431c81e0b29d1a419cfd0d8a776
xinykou <xinyi@stu.ecnu.edu.cn>
2024-07-03 23:15:02 +0800

    ablation for dataset_name for method: unaligned

---
+++ scripts/baselines/dataset_name/integrated_eval-unaligned_finetune.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts/alignment
+sub_dir=$(dirname "$parent_dir") # scripts
+main_dir=$(dirname "$sub_dir") # ./
+
+cd $main_dir
+
+#chmod +x ./scripts/baselines/dataset_name/unaligned_finetune.sh
+#
+#./scripts/baselines/dataset_name/unaligned_finetune.sh
+
+
+params=(
+      "5 gsm8k-unaligned-finetune n1000_p0.05"
+)
+
+#      "4 ag_news-unaligned-finetune n1000_p0.05"
+#      "5 gsm8k-unaligned-finetune n1000_p0.05"
+
+chmod +x ./scripts/baselines/dataset_name/eval_parallel.sh
+
+# Run the script in parallel
+for param in "${params[@]}"
+do
+    ./scripts/baselines/dataset_name/eval_parallel.sh $param
+done
+
+wait
+
+echo "All eval done!"
