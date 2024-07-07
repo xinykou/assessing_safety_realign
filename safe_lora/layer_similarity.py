45c38bd1578c01409860ce5532991aab6ae9872c
xinykou <xinyi@stu.ecnu.edu.cn>
2024-07-07 19:52:59 +0800

    layer_similarity

--- safe_lora/layer_similarity.py
+++ safe_lora/layer_similarity.py
@@ -7,7 +7,8 @@ def main():
     parser.add_argument("--first_lora_layer_distributions", default="")
     parser.add_argument("--second_lora_layer_distributions", default="")
     parser.add_argument("--output_path", default="")
-    parser.add_argument("--tau", default=0.5, type=float)
+    parser.add_argument("--tau", default=None, type=float)
+    parser.add_argument("--prune_rate", default=None, type=float)
     parser.add_argument("--sparsity_ratio", default=0.1, type=float)
     args = parser.parse_args()
 
@@ -18,23 +19,32 @@ def main():
     else:
         with open(args.first_lora_layer_distributions, "r") as f:
             for lin in f:
-                parts = lin.strip().split(",")
-                if len(parts) == 1:
-                    continue
+                if args.prune_rate is None:
+                    parts = lin.strip().split(",")
+                    if len(parts) == 1:
+                        continue
+                    else:
+                        first_record.append(parts[0])
                 else:
-                    first_record.append(parts[0])
+                    first_record.append(lin.strip())
 
         with open(args.second_lora_layer_distributions, "r") as f:
             for lin in f:
-                parts = lin.strip().split(",")
-                if len(parts) == 1:
-                    continue
+                if args.prune_rate is None:
+                    parts = lin.strip().split(",")
+                    if len(parts) == 1:
+                        continue
+                    else:
+                        second_record.append(parts[0])
                 else:
-                    second_record.append(parts[0])
+                    second_record.append(lin.strip())
 
     layer_similarity = []
     total_layers_num = len(first_record) if len(first_record) > len(second_record) else len(second_record)
-    save_name = f"sparsity_ratio_{args.sparsity_ratio}-layer_similarity.txt"
+    if args.tau is not None:
+        save_name = f"sparsity_ratio_{args.sparsity_ratio}-layer_similarity.txt"
+    elif args.prune_rate is not None:
+        save_name = f"prune_rate_{args.prune_rate}-layer_similarity.txt"
     save_path = os.path.join(args.output_path, save_name)
 
     for name in first_record:
@@ -46,12 +56,22 @@ def main():
         os.makedirs(args.output_path, exist_ok=True)
     if not os.path.exists(save_path):
         with open(save_path, "w") as f:
-            f.write(f"Similarity ratio (tau{args.tau}): {similarity_ratio}\n")
+            if args.tau is not None:
+                f.write(f"Similarity ratio (tau{args.tau}): {similarity_ratio}\n")
+            elif args.prune_rate is not None:
+                f.write(f"Similarity ratio (prune_rate{args.prune_rate}): {similarity_ratio}\n")
     else:
         with open(save_path, "a") as f:
-            f.write(f"Similarity ratio (tau{args.tau}): {similarity_ratio} \n")
+            if args.tau is not None:
+                f.write(f"Similarity ratio (tau{args.tau}): {similarity_ratio} \n")
+            elif args.prune_rate is not None:
+                f.write(f"Similarity ratio (prune_rate{args.prune_rate}): {similarity_ratio} \n")
 
-    print(f"Similarity ratio (tau{args.tau}): {similarity_ratio}")
+    if args.tau is not None:
+        print(f"Similarity ratio (tau{args.tau}): {similarity_ratio}")
+
+    elif args.prune_rate is not None:
+        print(f"Similarity ratio (prune_rate{args.prune_rate}): {similarity_ratio}")
 
 
 if __name__ == "__main__":
