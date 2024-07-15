33d979dc3a91e1b715d76a08285db2ed2405a70c
xinykou <xinyi@stu.ecnu.edu.cn>
2024-07-15 21:58:59 +0800

    search regions by wandg

---
+++ scripts/ablations/methods_to_search_regions/wandg/expo-adaptive_mask_replace-realign_dpo.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts/ablations/methods_to_search_regions
+sub_dir=$(dirname "$parent_dir") # ./scripts/ablations
+main_dir=$(dirname "$sub_dir") # ./scripts
+main_dir=$(dirname "$main_dir") # ./
+
+cd $main_dir
+# build alignment matrix
+export CUDA_VISIBLE_DEVICES=1
+
+
+dataset_name="sst2"
+dataset_selected="n1000_p0.05"
+fusion_effect=sft_to_dpo-alpha_0.9
+region_method=wandg  # wanda, wandg, or low_rank
+
+# 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+sparsity_ratios=(0.8)
+prune_rates=(0.5)
+epsilon=0.2
+# shellcheck disable=SC2068
+for sparsity_ratio in ${sparsity_ratios[@]}; do
+    echo "----->Running with sparsity_ratio=$sparsity_ratio"
+
+
+# realign lora
+for prune_rate in ${prune_rates[@]}; do
+    echo "----->Running with prune_rate=$prune_rate"
+    python ./safe_lora/identify_realign.py \
+         --model_path ./saves/lora/sft/checkpoint-125-merged \
+         --lora_path ./saves/lora/baselines/poison_ratio/aligned-finetune-${dataset_selected} \
+         --aligned_path ./saves/lora/expo_dpo_lora/${fusion_effect} \
+         --mask_path ./saves/lora/prune_regions/dpo-${region_method}-${sparsity_ratio}/mask_bottom_${sparsity_ratio}.pt \
+         --sparsity_ratio "${sparsity_ratio}" \
+         --prune_rate "${prune_rate}" \
+         --epsilon ${epsilon} \
+         --realign_type adaptive_mask_replace \
+         --output_path ./saves/lora/realign/expo-adaptive_mask_replace-safe_lora/${dataset_name}-dpo-${dataset_selected}-${region_method}
+
+
+done
+
+done
