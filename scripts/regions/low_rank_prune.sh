fceb732c9579a9b0f622ac090957ba9063200f7c
xinykou <xinyi@stu.ecnu.edu.cn>
2024-07-17 16:22:37 +0800

    search safety regions method: low_rank

--- scripts/regions/low_rank_prune.sh
+++ scripts/regions/low_rank_prune.sh
@@ -37,8 +37,8 @@ do
           modified_alignment_name="${alignment_name}"
       fi
       python ./prune_regions/sparsity_ratio_low.py \
-            --rank_path ./saves/lora/prune_regions/${modified_alignment_name}-${prune_type}-${sparsity_ratio}/rank_bottom_${sparsity_ratio} \
-            --output_dir ./saves/lora/prune_regions/${modified_alignment_name}-${prune_type}-${sparsity_ratio} \
+            --rank_path ./saves/lora/prune_regions/${alignment_name}-${prune_type}-${sparsity_ratio}/rank_bottom_${sparsity_ratio} \
+            --output_dir ./saves/lora/prune_regions/${alignment_name}-${prune_type}-${sparsity_ratio} \
             --sparsity_ratio ${sparsity_ratio}
     done
 done
