6751f992dfd1b7b2a42676cf3954a3a654db3ebd
xinykou <xinyi@stu.ecnu.edu.cn>
2024-07-23 09:27:36 +0800

    realignment method:safe_lora

--- scripts/baselines/alignment_techniques/integrated_eval-safelora_finetune.sh
+++ scripts/baselines/alignment_techniques/integrated_eval-safelora_finetune.sh
@@ -14,20 +14,17 @@ main_dir=$(dirname "$sub_dir") # ./
 cd $main_dir
 
 
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 dataset_selected=n1000_p0.05
 alignment_methods=("sft" "orpo" "kto" "simpo")
-
-chmod +x ./scripts/baselines/alignment_techniques/safelora_finetune.sh
-
-./scripts/baselines/alignment_techniques/safelora_finetune.sh
+taus=(0.6)
 
 
 # Run the script
 # shellcheck disable=SC2034
 for align_m in "${alignment_methods[@]}"
 do
-    for tau in $(seq 0.1 0.1 0.9); do
+    for tau in "${taus[@]}";do
         echo "------> Running with tau=$tau"
         python ./evaluation/poison/pred.py \
               --model_folder ./saves/lora/sft/checkpoint-125-merged \
@@ -37,21 +34,26 @@ do
               --end 1000 \
               --output_path ./results/lora/baselines/alignment_techniques/safelora-finetune-"${align_m}"-${dataset_selected}/tau_"${tau}"-safety.json \
 
-#        python ./evaluation/poison/eval_safety.py \
-#              --safety_evaluator_path ./pretrained_model/beaver-dam-7b \
-#              --input_path ./results/lora/baselines/alignment_techniques/safelora-finetune-"${align_m}"-${dataset_selected}/tau_${tau}-safety.json \
-#              --add
-
-        python ./evaluation/downstream_task/sst2_eval.py \
-              --model_folder ./saves/lora/sft/checkpoint-125-merged \
-              --lora_folder ./saves/lora/baselines/alignment_techniques/safelora-finetune-"${align_m}"-${dataset_selected}/tau_"${tau}" \
-              --start 0 \
-              --end 1000 \
-              --output_path ./results/lora/baselines/alignment_techniques/safelora-finetune-"${align_m}"-${dataset_selected}/tau_${tau}-downstream.json \
+        python ./evaluation/poison/eval_safety.py \
+              --safety_evaluator_path ./pretrained_model/beaver-dam-7b \
+              --input_path ./results/lora/baselines/alignment_techniques/safelora-finetune-"${align_m}"-${dataset_selected}/tau_${tau}-safety.json \
               --add
 
     done
 done
 
+
+# eval downstream task
+for align_m in "${alignment_methods[@]}"; do
+    for tau in "${taus[@]}"; do
+            python ./evaluation/downstream_task/sst2_eval.py \
+                --model_folder ./saves/lora/sft/checkpoint-125-merged \
+                --lora_folder ./saves/lora/baselines/alignment_techniques/safelora-finetune-"${align_m}"-${dataset_selected}/tau_"${tau}" \
+                --start 0 \
+                --end 1000 \
+                --output_path ./results/lora/baselines/alignment_techniques/safelora-finetune-"${align_m}"-${dataset_selected}/tau_${tau}-downstream.json \
+                --add
+    done
+done
 
 echo "All eval done!"
--- scripts/baselines/alignment_techniques/safelora_finetune.sh
+++ scripts/baselines/alignment_techniques/safelora_finetune.sh
@@ -7,23 +7,23 @@ current_script_path=$(realpath "$0")
 current_script_dir=$(dirname "$current_script_path")
 
 # 获取上一级目录
-parent_dir=$(dirname "$current_script_dir")  # scripts/safet_lora
+parent_dir=$(dirname "$current_script_dir")  # scripts/baselines/
 sub_dir=$(dirname "$parent_dir") # ./scripts
 main_dir=$(dirname "$sub_dir") # ./
 
 cd $main_dir
 
 # build alignment matrix
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=1
 
 dataset_selected=n1000_p0.05
 alignment_methods=("sft" "orpo" "kto" "simpo")
-
+taus=(0.6)
 # shellcheck disable=SC2068
 for align_m in ${alignment_methods[@]}; do
-
+echo "Running with alignment_method=$align_m"
 # realign lora
-for tau in $(seq 0.1 0.1 0.9); do
+for tau in ${taus[@]}; do
     echo "Running with tau=$tau"
     python ./safe_lora/identify_realign.py \
          --model_path ./saves/lora/sft/checkpoint-125-merged \
