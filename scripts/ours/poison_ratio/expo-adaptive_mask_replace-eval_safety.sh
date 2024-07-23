f38e6d21f896da7fe89644f3932c89645fb2ee62
xinykou <xinyi@stu.ecnu.edu.cn>
2024-07-23 15:35:41 +0800

    our method

---
+++ scripts/ours/poison_ratio/expo-adaptive_mask_replace-eval_safety.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts/ours
+sub_dir=$(dirname "$parent_dir") # ./scripts
+main_dir=$(dirname "$sub_dir") # ./
+
+cd "$main_dir"
+
+
+dataset_name=sst2
+alignment_name=expo_dpo_lora
+region_method=low_rank
+data_selected_all=("n1000_p0.01" "n1000_p0.05" "n1000_p0.1" "n1000_p0.2" "n1000_p0.3")
+model_path=./saves/lora/realign/expo-adaptive_mask_replace-safe_lora
+#
+
+export CUDA_VISIBLE_DEVICES=0
+
+# 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+sparsity_ratio=0.8
+prune_rates=(0.5)
+epsilon=0.2
+# 生成预测文件
+for data_selected in "${data_selected_all[@]}"; do
+    echo "-----> Running with data_selected=$data_selected"
+
+    for prune_rate in "${prune_rates[@]}"; do
+        echo "------> Running with prune_rate=$prune_rate"
+        python ./evaluation/poison/pred.py \
+            --model_folder ./saves/lora/sft/checkpoint-125-merged \
+            --lora_folder ${model_path}/${dataset_name}-${alignment_name}-${data_selected}-${region_method}/sparsity_ratio_"${sparsity_ratio}"_prune_rate_"${prune_rate}"_epsilon_"${epsilon}" \
+            --instruction_path BeaverTails \
+            --start 0 \
+            --end 1000 \
+            --output_path ./results/lora/realign/expo-adaptive_mask_replace-safe_lora/${dataset_name}-${alignment_name}-${data_selected}-${region_method}/sparsity_ratio_"${sparsity_ratio}"_prune_rate_"${prune_rate}"_epsilon_"${epsilon}"-safety.json
+
+        # 将文件列表传递给 Python 脚本
+        python ./evaluation/poison/eval_safety.py \
+              --safety_evaluator_path ./pretrained_model/beaver-dam-7b \
+              --input_path ./results/lora/realign/expo-adaptive_mask_replace-safe_lora/${dataset_name}-${alignment_name}-${data_selected}-${region_method}/sparsity_ratio_"${sparsity_ratio}"_prune_rate_"${prune_rate}"_epsilon_"${epsilon}"-safety.json \
+              --add
+
+    done
+done
+
+
+
---
+++ scripts/ours/poison_ratio/expo-adaptive_mask_replace-realign.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts/safe_lora
+sub_dir=$(dirname "$parent_dir") # ./scripts
+main_dir=$(dirname "$sub_dir") # ./
+
+cd $main_dir
+# build alignment matrix
+export CUDA_VISIBLE_DEVICES=1
+
+
+dataset_name="sst2"
+dataset_selected_all=("n1000_p0.01" "n1000_p0.05" "n1000_p0.1" "n1000_p0.2" "n1000_p0.3")
+region_method=low_rank  # wanda, wandg, or low_rank
+
+# 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9
+sparsity_ratio=0.8
+prune_rates=(0.5)
+epsilon=0.2
+fusion_effect=sft_to_dpo-alpha_0.9
+alignment_name=expo_dpo_lora
+# shellcheck disable=SC2068
+for dataset_selected in ${dataset_selected_all[@]}; do
+    echo "----->Running with dataset_selected=$dataset_selected"
+
+# realign lora
+for prune_rate in ${prune_rates[@]}; do
+    echo "----->Running with prune_rate=$prune_rate"
+    python ./safe_lora/identify_realign.py \
+         --model_path ./saves/lora/sft/checkpoint-125-merged \
+         --lora_path ./saves/lora/baselines/poison_ratio/aligned-finetune-${dataset_selected} \
+         --aligned_path ./saves/lora/"${alignment_name}"/${fusion_effect} \
+         --mask_path ./saves/lora/prune_regions/${alignment_name}-${region_method}-${sparsity_ratio}/mask_bottom_${sparsity_ratio}.pt \
+         --sparsity_ratio "${sparsity_ratio}" \
+         --prune_rate "${prune_rate}" \
+         --epsilon ${epsilon} \
+         --realign_type adaptive_mask_replace \
+         --output_path ./saves/lora/realign/expo-adaptive_mask_replace-safe_lora/${dataset_name}-${alignment_name}-${dataset_selected}-${region_method}
+
+
+done
+
+done
