b8ecb3afa293b06d96b110bfa0b4d774f5189e7e
xinykou <xinyi@stu.ecnu.edu.cn>
2024-08-01 21:38:01 +0800

    results for clean dataset

---
+++ scripts/baselines/clean_data/integrated_eval-aligned_finetune.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# 获取当前脚本的绝对路径
+current_script_path=$(realpath "$0")
+
+# 获取当前脚本所在的目录
+current_script_dir=$(dirname "$current_script_path")
+
+# 获取上一级目录
+parent_dir=$(dirname "$current_script_dir")  # scripts/alignment
+sub_dir=$(dirname "$parent_dir") # scripts
+main_dir=$(dirname "$sub_dir") # ./
+
+cd $main_dir
+
+params=(
+    "0 aligned-finetune n1000_p0.01"
+    "1 aligned-finetune n1000_p0.05"
+    "2 aligned-finetune n1000_p0.1"
+    "3 aligned-finetune n1000_p0.2"
+    "1 aligned-finetune n1000_p0.3"
+
+)
+
+chmod +x ./scripts/baselines/clean_data/eval_parallel.sh
+
+# Run the script in parallel
+for param in "${params[@]}"
+do
+    ./scripts/baselines/clean_data/eval_parallel.sh $param
+done
+
+wait
+
+echo "All eval done!"
